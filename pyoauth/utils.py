3589d159e284bac33b4be092e1398464cc9f32d1
Gora Khargosh <gora.khargosh@gmail.com>
2011-06-23 05:54:06 +0530

    Adds routine to generate OAuth verifier code.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- pyoauth/utils.py
+++ pyoauth/utils.py
@@ -95,6 +95,25 @@ def oauth_generate_nonce():
     return binascii.b2a_hex(uuid.uuid4().bytes)
 
 
+def oauth_generate_verifier(length=8):
+    """
+    Calculates an OAuth verifier code.
+
+    The verifier code will be displayed by the server if a callback URL
+    is not provided by the client. The resource owner (the end-user) may
+    need to enter this verifier code on a limited device. Therefore,
+    we limit the length of this code to 8 characters to keep it suitable
+    for manual entry.
+
+    :param length:
+        Length of the verifier code. Defaults to 8.
+    :returns:
+        A string representation of a randomly-generated hexadecimal OAuth
+        verifier code.
+    """
+    return oauth_generate_nonce()[:length]
+
+
 def oauth_generate_timestamp():
     """
     Generates an OAuth timestamp.
