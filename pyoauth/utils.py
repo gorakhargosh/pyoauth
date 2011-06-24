5d85941538a185cf7893ef2d18a6a34c2e2e22a3
Gora Khargosh <gora.khargosh@gmail.com>
2011-06-24 06:17:36 +0530

    Make authorization header parameter indentation more obvious.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- pyoauth/utils.py
+++ pyoauth/utils.py
@@ -836,12 +836,14 @@ def oauth_get_normalized_authorization_header_value(query_params, realm=None):
     :returns:
         A properly formatted Authorization header value.
     """
+    indentation = " " * len("Authorization: ")
     if realm:
-        s = 'OAuth realm="' + str(realm) + '",\n               '
+        s = 'OAuth realm="' + str(realm) + '",\n' + indentation
     else:
         s = 'OAuth '
     normalized_param_pairs = _oauth_get_normalized_query_param_pairs_l(query_params, ignored_names=("realm"))
-    s += ",\n               ".join([k+'="'+v+ '"' for k, v in normalized_param_pairs])
+    delimiter = ",\n" + indentation
+    s += delimiter.join([k+'="'+v+ '"' for k, v in normalized_param_pairs])
     return s
 
 
