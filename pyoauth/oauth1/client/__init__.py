02bede4961eddb007b4beee13c7bcf3fcf2dbec8
Gora Khargosh <gora.khargosh@gmail.com>
2011-06-29 20:19:34 +0530

    Client request method argument validation and refactoring.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- pyoauth/oauth1/client/__init__.py
+++ pyoauth/oauth1/client/__init__.py
@@ -100,34 +100,34 @@ class Client(object):
 
     def build_temporary_credentials_request(self,
                                             method,
-                                            payload_params=None,
+                                            query_params=None,
+                                            headers=None,
                                             realm=None,
                                             oauth_signature_method=SIGNATURE_METHOD_HMAC_SHA1,
                                             oauth_callback=None,
                                             **extra_oauth_params):
         oauth_request_url = self._temporary_credentials_request_uri
         return self._build_request(method=method,
-                                       oauth_request_url=oauth_request_url,
-                                       payload_params=payload_params,
-                                       realm=realm,
-                                       oauth_signature_method=oauth_signature_method,
-                                       oauth_callback=oauth_callback,
-                                       **extra_oauth_params)
+                                   oauth_request_url=oauth_request_url,
+                                   query_params=query_params,
+                                   headers=headers,
+                                   realm=realm,
+                                   oauth_signature_method=oauth_signature_method,
+                                   oauth_callback=oauth_callback,
+                                   **extra_oauth_params)
 
     def parse_temporary_credentials_response(self, response):
-        if not isinstance(response, Response):
-            raise ValueError("``response`` must be of type pyoauth.http.Response")
-        if response.error:
-            raise ValueError("Could not fetch temporary credentials -- HTTP status code: %d" % response.status_code)
-        if not response.body:
-            raise ValueError("OAuth server did not return a valid response")
-        if response.get_content_type() != CONTENT_TYPE_FORM_URLENCODED:
-            raise ValueError("OAuth server must return Content-Type: `%s`" % CONTENT_TYPE_FORM_URLENCODED)
-
-        # The response body must be URL encoded.
+        self._validate_oauth_response(response)
         params = oauth_parse_qs(response.body)
         return params, Credentials(identifier=params["oauth_token"],
                                    shared_secret=params["oauth_token_secret"])
+
+    def parse_token_credentials_response(self, response):
+        self._validate_oauth_response(response)
+        params = oauth_parse_qs(response.body)
+        return params, Credentials(identifier=params["oauth_token"],
+                                   shared_secret=params["oauth_token_secret"])
+
 
     def get_authorization_url(self, temporary_credentials, **query_params):
         url = self._resource_owner_authorization_uri
@@ -136,10 +136,23 @@ class Client(object):
             url = oauth_url_append_query_params(url, query_params)
         return oauth_url_append_query_params(url, dict(oauth_token=temporary_credentials.identifier))
 
+    def _validate_oauth_response(self, response):
+        if not isinstance(response, Response):
+            raise ValueError("``response`` must be of type pyoauth.http.Response")
+        if response.error:
+            raise ValueError("Could not fetch temporary credentials -- HTTP status code: %d" % response.status_code)
+        if not response.body:
+            raise ValueError("OAuth server did not return a valid response")
+        # The response body must be URL encoded.
+        if response.get_content_type() != CONTENT_TYPE_FORM_URLENCODED:
+            raise ValueError("OAuth server must return Content-Type: `%s`" % CONTENT_TYPE_FORM_URLENCODED)
+
+
     def _build_request(self,
                       method,
                       oauth_request_url,
-                      payload_params=None,
+                      query_params=None,
+                      headers=None,
                       realm=None,
                       oauth_signature_method=SIGNATURE_METHOD_HMAC_SHA1,
                       **extra_oauth_params):
@@ -148,10 +161,16 @@ class Client(object):
 
         :param method:
             HTTP request method.
-        :param payload_params:
+        :param query_params:
             A dictionary of payload parameters. These will be serialized
             into the URL or the entity-body depending on the HTTP request method.
-            These must not include any parameters starting with "oauth_".
+            These must not include any parameters starting with "oauth_". Any
+            of these parameters with names starting with the "oauth_" prefix
+            will be ignored.
+        :param headers:
+            A dictionary of headers that will be passed along with the request.
+            If the "Authorization" header is included, its value will be set
+            to the authorization header value generated by this function.
         :param realm:
             The value to use for the realm parameter in the Authorization HTTP
             header. It will be excluded from the base string, however.
@@ -163,10 +182,19 @@ class Client(object):
             ``SIGNATURE_METHOD_RSA_SHA1``, or ``SIGNATURE_METHOD_PLAINTEXT``.
         :param extra_oauth_params:
             Any additional oauth parameters you would like to include.
-            The parameter names must begin with "oauth_" otherwise they will
-            not be included.
+            The parameter names must begin with "oauth_". Any other parameters
+            with names that do not begin with this prefix will be ignored.
         """
-        # Common OAuth params.
+        method = method.upper()
+        headers = headers or {}
+        realm = realm or ""
+
+        if oauth_signature_method not in SIGNATURE_METHOD_MAP:
+            raise ValueError("Invalid signature method specified -- `%r`" % (oauth_signature_method,))
+
+
+        # Required OAuth protocol parameters.
+        # See Making Requests (http://tools.ietf.org/html/rfc5849#section-3.1)
         oauth_params = dict(
             oauth_consumer_key=self._client_credentials.identifier,
             oauth_signature_method=oauth_signature_method,
@@ -174,19 +202,26 @@ class Client(object):
             oauth_nonce=oauth_generate_nonce(),
             oauth_version=self.oauth_version,
         )
-        method = method.upper()
 
         # Filter and add additional OAuth parameters.
         extra_oauth_params = oauth_protocol_params_sanitize(extra_oauth_params)
         for k, v in extra_oauth_params.items():
-            if k in ("oauth_signature",):
-                logging.warning("Specified additional protocol parameter `%r` will be ignored.", k)
-                continue
-            elif k == "oauth_callback" and v:
-                # Set a callback URL only if it is available.
-                oauth_params["oauth_callback"] = v
+            if k in ("oauth_signature",
+                     "oauth_nonce",
+                     "oauth_timestamp",
+                     "oauth_consumer_key",
+                     "oauth_version",       # If specified, MUST be set to "1.0", which is what our class does.
+                    ):
+                # Don't override these required system-generated protocol parameters.
+                raise ValueError("Cannot override system-generated protocol parameter `%r`." % k)
+            elif k == "oauth_callback":
+                if v:
+                    # Set a callback URL only if it is available.
+                    oauth_params["oauth_callback"] = v
+                else:
+                    raise ValueError("oauth_callback parameter value is undefined.")
             else:
-                if oauth_params.has_key(k):
+                if k in oauth_params:
                     # Warn when an existing protocol parameter is being
                     # overridden.
                     logging.warning("Overriding existing protocol parameter `%r`=`%r` with `%r`=`%r`",
@@ -194,30 +229,39 @@ class Client(object):
                 oauth_params[k] = v
 
         # Filter payload parameters for the request.
-        payload_params = oauth_url_query_params_sanitize(payload_params)
+        query_params = oauth_url_query_params_sanitize(query_params)
 
         # Determine the request's OAuth signature.
-        signature_url = oauth_url_query_params_add(oauth_request_url, payload_params)
+        signature_url = oauth_url_query_params_add(oauth_request_url, query_params)
         oauth_params["oauth_signature"] = self._sign_request_data(oauth_signature_method,
                                                                   method, signature_url, oauth_params)
 
         # Build request data now.
-        request_headers = {}
-        if self._use_authorization_header:
+        # OAuth parameters and any parameters starting with the "oauth_"
+        # must be included only in ONE of these three locations:
+        #
+        # 1. Authorization header.
+        # 2. Request URI query string.
+        # 3. Request entity body.
+        #
+        # See Parameter Transmission (http://tools.ietf.org/html/rfc5849#section-3.6)
+        if self._use_authorization_header or headers.has_key("Authorization"):
             auth_header_value = oauth_get_normalized_authorization_header_value(oauth_params, realm=realm)
-            request_headers["Authorization"] = auth_header_value
+            headers["Authorization"] = auth_header_value
+            # Empty the params so that they are not used below.
+            oauth_params = None
 
         if method == "GET":
             request_url = oauth_url_append_query_params(signature_url, oauth_params)
-            request_payload = ""
+            payload = ""
         elif method == "POST":
             request_url = oauth_request_url
-            request_headers["Content-Type"] = CONTENT_TYPE_FORM_URLENCODED
-            request_payload = oauth_urlencode_s(payload_params)
+            headers["Content-Type"] = CONTENT_TYPE_FORM_URLENCODED
+            payload = oauth_urlencode_s(query_params)
         else:
             raise NotImplementedError("Not implemented any other HTTP methods yet.")
 
-        return Request(method, url=request_url, payload=request_payload, headers=request_headers)
+        return Request(method, url=request_url, payload=payload, headers=headers)
 
     def _sign_request_data(self, signature_method,
              method, url, oauth_params,
