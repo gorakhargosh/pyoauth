5dd7abbbf8ae29913c018a3b77ea479d55439928
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-06 11:43:30 +0530

    Fixes broken import.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- pyoauth/crypto/RSAKey/PyCrypto.py
+++ pyoauth/crypto/RSAKey/PyCrypto.py
@@ -7,7 +7,7 @@ from pyoauth.crypto.utils.bytearray import bytearray_to_string
 from pyoauth.crypto.utils.cryptomath import *
 
 from pyoauth.crypto.RSAKey import RSAKey
-from pyoauth.crypto.pure import Python_RSAKey
+from pyoauth.crypto.RSAKey.pure import Python_RSAKey
 
 if pycryptoLoaded:
 
