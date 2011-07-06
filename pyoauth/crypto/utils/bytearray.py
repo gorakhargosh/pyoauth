d12da6370a814d4af45a74d5fabbd5bfa758c9a6
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-06 11:14:57 +0530

    Adds documentation to the bytearray.py module.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- pyoauth/crypto/utils/bytearray.py
+++ pyoauth/crypto/utils/bytearray.py
@@ -4,23 +4,72 @@
 """
 :module: pyoauth.crypto.utils.bytearray
 :synopsis: Byte arrays.
+
+Functions:
+----------
+.. autofunction:: bytearray_create
+.. autofunction:: bytearray_create_zeros
+.. autofunction:: bytearray_concat
+.. autofunction:: bytearray_to_string
+.. autofunction:: bytearray_from_string
+
 """
 
-import array
+from array import array
 
-def bytearray_create(seq):
-    return array.array('B', seq)
+def bytearray_create(sequence):
+    """
+    Creates a byte array from a given sequence.
 
-def bytearray_create_zeros(howMany):
-    return array.array('B', [0] * howMany)
+    :param sequence:
+        The sequence from which a byte array will be created.
+    :returns:
+        A byte array.
+    """
+    return array('B', sequence)
 
-def bytearray_concat(a1, a2):
-    return a1+a2
+def bytearray_create_zeros(count):
+    """
+    Creates a zero-filled byte array of with ``count`` bytes.
 
-def bytearray_to_string(bytes):
-    return bytes.tostring()
+    :param count:
+        The number of zero bytes.
+    :returns:
+        Zero-filled byte array.
+    """
+    return array('B', [0] * count)
 
-def bytearray_from_string(s):
-    bytes = bytearray_create_zeros(0)
-    bytes.fromstring(s)
-    return bytes
+def bytearray_concat(arr1, arr2):
+    """
+    Concatenates two byte arrays.
+
+    :param arr1:
+        Byte array 1
+    :param arr2:
+        Byte array 2
+    :returns:
+        Concatenated byte array.
+    """
+    return arr1 + arr2
+
+def bytearray_to_string(arr):
+    """
+    Converts a byte array into a string.
+
+    :param arr:
+        The byte array.
+    :returns:
+        String.
+    """
+    return arr.tostring()
+
+def bytearray_from_string(value):
+    """
+    Converts a string into a byte array.
+
+    :param value:
+        String value.
+    :returns:
+        Byte array.
+    """
+    return bytearray_create_zeros(0).fromstring(value)
