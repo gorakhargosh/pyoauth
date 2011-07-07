80cd169a54ab098c96e8513090ec0045cf964bd7
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-07 06:30:13 +0530

    Don't duplicate code.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- pyoauth/crypto/RSA/__init__.py
+++ pyoauth/crypto/RSA/__init__.py
@@ -20,9 +20,8 @@
 from pyasn1.type import univ, namedtype
 from pyasn1.codec.der import decoder, encoder
 from pyoauth.crypto.codec.pemder import pem_to_der_public_key, der_to_pem_public_key
-from pyoauth.crypto.utils.bitstring import bits_to_long
 from pyoauth.crypto.codec.x509 import SubjectPublicKeyInfo
-
+from pyoauth.crypto.X509 import X509Certificate
 
 class RSAPublicKey(object):
     # http://tools.ietf.org/html/rfc3279 - Section 2.3.1
@@ -52,17 +51,7 @@ class RSAPublicKey(object):
         :returns:
             Tuple of (modulus, exponent)
         """
-        public_key_long = bits_to_long(public_key_bitstring)
-        public_key_hex = hex(public_key_long)[2:-1]
-        public_key_asn1 = decoder.decode(public_key_hex.decode('hex'))
-
-        if len(public_key_asn1) < 1:
-            raise ValueError("Problem ASN.1 decoding public key bytes")
-
-        if len(public_key_asn1[0]) < 2:
-            raise ValueError("Couldn't obtain RSA modulus and exponent from public key.")
-
-        return long(public_key_asn1[0][0]), long(public_key_asn1[0][1])
+        X509Certificate.parse_public_rsa_key_bits(public_key_bitstring)
 
     @classmethod
     def decode_from_pem_key(cls, key):
