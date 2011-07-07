2d8b50a99a0704cd4c28c35aab2c350180a7a0f3
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-07 08:19:03 +0530

    Adds another reference link
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- pyoauth/crypto/codec/__init__.py
+++ pyoauth/crypto/codec/__init__.py
@@ -22,6 +22,7 @@
 :author: Arne Roomann-Kurrik <kurrik@gmail.com>
 :author: Yesudeep Mangalapilly <yesudeep@gmail.com>
 :see: http://code.google.com/p/kurrik-resources/source/browse/trunk/src/x509/x509.py?r=14
+:see: http://blog.roomanna.com/12-12-2010/packaging-chrome-extensions
 
 Classes:
 --------
