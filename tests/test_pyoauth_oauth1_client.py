fc1dba8bad2bef43bcdbfc0b11af90f765aa1f94
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-26 04:15:13 +0530

    Adds tests for _OAuthClient._generate_signature
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

---
+++ tests/test_pyoauth_oauth1_client.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest2
+from pyoauth.error import InvalidSignatureMethodError
+from pyoauth.protocol import generate_base_string
+from pyoauth.oauth1.client import _OAuthClient
+
+class Test__OAuthClient__generate_signature(unittest2.TestCase):
+    def test_generates_signature(self):
+        params = {
+            "method": "POST",
+            "realm": "Photos",
+            "url": "https://photos.example.net/initiate",
+            "oauth_consumer_secret": "kd94hf93k423kf44",
+            "oauth_token_secret": None,
+            "oauth_signature": "74KNZJeDHnMBp0EMJ9ZHt/XKycU=",
+            "oauth_params": dict(
+                oauth_consumer_key="dpf43f3p2l4k3l03",
+                oauth_signature_method="HMAC-SHA1",
+                oauth_timestamp="137131200",
+                oauth_nonce="wIjqoS",
+                oauth_callback="http://printer.example.com/ready",
+            )
+        }
+        self.assertEqual(_OAuthClient._generate_signature(
+            params["method"],
+            params["url"],
+            None,
+            params["oauth_consumer_secret"],
+            params["oauth_token_secret"],
+            params["oauth_params"],
+        ), params["oauth_signature"])
+
+    def test_raises_InvalidSignatureMethodError_when_invalid_signature_method(self):
+        params = {
+            "method": "POST",
+            "url": "https://photos.example.net/initiate",
+            "oauth_consumer_secret": "kd94hf93k423kf44",
+            "oauth_token_secret": None,
+            "oauth_params": dict(
+                oauth_consumer_key="dpf43f3p2l4k3l03",
+                oauth_signature_method="HMAC-SHOOOOOOOOOOOOOOO1",
+                oauth_timestamp="137131200",
+                oauth_nonce="wIjqoS",
+                oauth_callback="http://printer.example.com/ready",
+            )
+        }
+        self.assertRaises(InvalidSignatureMethodError,
+              _OAuthClient._generate_signature,
+              params["method"],
+              params["url"],
+              None,
+              params["oauth_consumer_secret"],
+              params["oauth_token_secret"],
+              params["oauth_params"],
+        )
+
+    def test_raises_KeyError_when_missing_signature_method(self):
+        params = {
+            "method": "POST",
+            "url": "https://photos.example.net/initiate",
+            "oauth_consumer_secret": "kd94hf93k423kf44",
+            "oauth_token_secret": None,
+            "oauth_params": dict(
+                oauth_consumer_key="dpf43f3p2l4k3l03",
+                oauth_timestamp="137131200",
+                # oauth_signature_method="HMAC-SHOOOOOOOOOOOOOOO1",
+                oauth_nonce="wIjqoS",
+                oauth_callback="http://printer.example.com/ready",
+            )
+        }
+        self.assertRaises(KeyError, _OAuthClient._generate_signature,
+              params["method"],
+              params["url"],
+              None,
+              params["oauth_consumer_secret"],
+              params["oauth_token_secret"],
+              params["oauth_params"],
+        )
+
