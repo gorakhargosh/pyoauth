071208a574e03f9b234946c31812719a33963ab8
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-26 04:45:31 +0530

    Tests for _OAuthClient._generate_oauth_params
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- tests/test_pyoauth_oauth1_client.py
+++ tests/test_pyoauth_oauth1_client.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest2
-from pyoauth.error import InvalidSignatureMethodError
+from pyoauth.error import InvalidSignatureMethodError, IllegalArgumentError
 from pyoauth.oauth1.client import _OAuthClient
 from mom.builtins import is_bytes, is_bytes_or_unicode
 
@@ -33,6 +33,68 @@ class Test__OAuthClient_generate_timestamp(unittest2.TestCase):
         self.assertTrue(len(_OAuthClient.generate_timestamp()) > 0,
                     "Timestamp is an empty string.")
 
+class Test__OAuthClient__generate_oauth_params(unittest2.TestCase):
+    def test_generates_oauth_params(self):
+        args_no_token = dict(
+            oauth_consumer_key="consumer-key",
+            oauth_signature_method="HMAC-SHA1",
+            oauth_version="1.0",
+            oauth_nonce="nonce",
+            oauth_timestamp="timestamp",
+            oauth_token=None,
+            oauth_extra="extra-parameter",
+        )
+        args_with_token = dict(
+            oauth_consumer_key="consumer-key",
+            oauth_signature_method="HMAC-SHA1",
+            oauth_version="1.0",
+            oauth_nonce="nonce",
+            oauth_timestamp="timestamp",
+            oauth_token="token",
+            oauth_extra="extra-parameter",
+        )
+
+        args1 = dict(
+            non_oauth_param="ignore-this",
+        )
+        args1.update(args_no_token)
+        del args_no_token["oauth_token"]
+        self.assertDictEqual(_OAuthClient._generate_oauth_params(**args1),
+                             args_no_token)
+
+        args2 = dict(
+            non_oauth_param="ignore-this",
+        )
+        args2.update(args_with_token)
+        self.assertDictEqual(_OAuthClient._generate_oauth_params(**args2),
+                             args_with_token)
+
+    def test_InvalidSignatureMethodError_when_invalid_signature_method(self):
+        args_no_token = dict(
+            oauth_consumer_key="consumer-key",
+            oauth_signature_method="HMAC-SHOOOOOO1",
+            oauth_version="1.0",
+            oauth_nonce="nonce",
+            oauth_timestamp="timestamp",
+            oauth_token=None,
+            oauth_extra="extra-parameter",
+        )
+        self.assertRaises(InvalidSignatureMethodError,
+                          _OAuthClient._generate_oauth_params, **args_no_token)
+
+    def test_IllegalArgumentError_when_signature_specified(self):
+        args_no_token = dict(
+            oauth_consumer_key="consumer-key",
+            oauth_signature_method="HMAC-SHA1",
+            oauth_version="1.0",
+            oauth_nonce="nonce",
+            oauth_timestamp="timestamp",
+            oauth_token=None,
+            oauth_extra="extra-parameter",
+            oauth_signature="BOOM!"
+        )
+        self.assertRaises(IllegalArgumentError,
+                          _OAuthClient._generate_oauth_params, **args_no_token)
 
 class Test__OAuthClient__generate_signature(unittest2.TestCase):
     def test_generates_signature(self):
