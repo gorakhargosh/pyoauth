3cd158f631748ca86e393d325c07441f581c76e5
Gora Khargosh <gora.khargosh@gmail.com>
2011-07-26 04:20:21 +0530

    Default nonce and timestamp generator tests.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- tests/test_pyoauth_oauth1_client.py
+++ tests/test_pyoauth_oauth1_client.py
@@ -4,10 +4,35 @@
 import unittest2
 from pyoauth.error import InvalidSignatureMethodError
 from pyoauth.oauth1.client import _OAuthClient
+from mom.builtins import is_bytes, is_bytes_or_unicode
 
 class Test__OAuthClient_oauth_version(unittest2.TestCase):
     def test_default_is_1_0(self):
         self.assertEqual(_OAuthClient(None, None).oauth_version, "1.0")
+
+class Test__OAuthClient_generate_nonce(unittest2.TestCase):
+    def test_uniqueness(self):
+        self.assertNotEqual(_OAuthClient.generate_nonce(), _OAuthClient.generate_nonce())
+
+    def test_unsigned_integer(self):
+        self.assertTrue(int(_OAuthClient.generate_nonce()) >= 0)
+
+    def test_result_is_string(self):
+        self.assertTrue(is_bytes(_OAuthClient.generate_nonce()))
+
+class Test__OAuthClient_generate_timestamp(unittest2.TestCase):
+    def test_is_positive_integer_string(self):
+        self.assertTrue(int(_OAuthClient.generate_timestamp()) > 0,
+                    "Timestamp is not positive integer string.")
+
+    def test_is_string(self):
+        self.assertTrue(is_bytes_or_unicode(_OAuthClient.generate_timestamp()),
+                    "Timestamp is not a string.")
+
+    def test_is_not_empty_string(self):
+        self.assertTrue(len(_OAuthClient.generate_timestamp()) > 0,
+                    "Timestamp is an empty string.")
+
 
 class Test__OAuthClient__generate_signature(unittest2.TestCase):
     def test_generates_signature(self):
