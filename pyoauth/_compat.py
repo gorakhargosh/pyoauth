7f8d361ed498a4361dd576adbd06ca292731068c
Yesudeep Mangalapilly <yesudeep@google.com>
2012-03-03 11:30:28 +0530

    Updates copyright notices.
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@google.com>

--- pyoauth/_compat.py
+++ pyoauth/_compat.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Compatibility module.
 #
 # Copyright (C) 2011 Yesudeep Mangalapilly <yesudeep@gmail.com>
+# Copyright (C) 2012 Google, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -15,6 +15,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
 
 from __future__ import absolute_import
 
--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011 Yesudeep Mangalapilly <yesudeep@gmail.com>
+# Copyright (C) 2012 Google, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 
 """
 Python module that implements the OAuth protocol for clients
@@ -22,7 +38,7 @@ setup(
     description="Python OAuth implementation for clients and servers",
     long_description=__doc__,
     author="Yesudeep Mangalapilly",
-    author_email="yesudeep@gmail.com",
+    author_email="yesudeep@google.com",
     zip_safe=True,
     platforms="any",
     packages=["pyoauth"],
